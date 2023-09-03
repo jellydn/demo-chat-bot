85f42b732f0ced836cef97e6d9564509037f5945
Huynh Duc Dung <dunghd.it@gmail.com>
2023-09-03 21:49:17 +0800

    chore(deps): remove unused packages

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  dts: true,
-  external: ['react'],
-  format: ['cjs'],
-  esbuildOptions(options, context) {
-    options.assetNames = 'assets/[name]';
-  },
-  loader: {
-    '.svg': 'file',
-  },
+    entry: ['src/index.ts'],
+    dts: true,
+    external: ['react'],
+    format: ['cjs'],
+    esbuildOptions(options, _context) {
+        options.assetNames = 'assets/[name]';
+    },
+    loader: {
+        '.svg': 'dataurl',
+    },
 });
-
